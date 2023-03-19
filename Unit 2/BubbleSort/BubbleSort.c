0892ba1e1303024b3752398c4e5f61e0494ffabb
RahulB <rahulbisht1012@gmail.com>
2023-03-19 12:10:12 +0530

    Sorting Algo

---
+++ Unit 2/BubbleSort/BubbleSort.c
@@ -0,0 +1,47 @@
+// C program for implementation of Bubble sort
+#include <stdio.h>
+
+// A function to implement bubble sort
+void bubbleSort(int arr[], int n)
+{
+    int i, j;
+    for (i = 0; i < n - 1; i++)
+    {
+        // Last i elements are already in place
+        for (j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+// Driver program to test above functions
+int main()
+{
+    int arr[] = {5, 4, 3, 2, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("UnSorted array: \n");
+    printArray(arr, n);
+
+    bubbleSort(arr, n);
+
+    printf("Sorted array: \n");
+    printArray(arr, n);
+
+    return 0;
+}
---
+++ Unit 2/BubbleSort/Notes.md
@@ -0,0 +1,14 @@
+* Time Complexity: O(N^2)
+* Auxiliary Space: O(1) 
+
+* Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping 
+the adjacent elements if they are in the wrong order. This algorithm is not suitable 
+for large data sets as its average and worst-case time complexity is quite high.
+
+
+* Worst and Average Case Time Complexity: O(N2).
+* The worst case occurs when an array is reverse sorted.
+
+* Best Case Time Complexity: O(N).
+* The best case occurs when an array is already sorted.
+Auxiliary Space: O(1)

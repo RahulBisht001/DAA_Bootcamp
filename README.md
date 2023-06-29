4df805876f2df3b1034f0f0f90e938fb7a571f67
RahulB <rahulbisht1012@gmail.com>
2023-06-29 12:18:31 +0530

    README.md update

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # DAA-Bootcamp
 
-* Ace your Collage Exams
+* Binary Search Recurrence Relation : `T(n) = T(n/2) + 1` 
+where `T(n)` is the time required for binary search in an array of size n.
 
-Use These Commands to Run your Program on Terminal
-```
-1.  gcc <filename.c> -o <exeFilename(similar)>
+*  Recurrence Relation of Tower of Hanoi : `T(n) = 2* T(n -1) + 1`
 
-2.  ./<exeFileName>
+* Recurrence Relation of Quick Sort : `T(n) = T(n-1) + O(n)`
+because we need to sort the subArray of size `n-1` and partition the array of size `n` in
+`O(n)` time.
 
-```
+* Recurrence Relation of Merge Sort : `T(n)=2T(n/2) + An`
+
+* Recurrence Relation of Fibonacci series : `T(n) = T(n−1) + T(n−2)`
--- Unit 2/HeapSort/HeapSort.c
+++ Unit 2/HeapSort/HeapSort.c
@@ -37,20 +37,25 @@ void heapSort(int arr[], int n)
     }
 }
 
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
 int main()
 {
-    int arr[] = {12, 11, 13, 5, 6, 7};
+    int arr[] = {12, -11, -13, 5, -6, 7};
     int n = sizeof(arr) / sizeof(arr[0]);
 
-    printf("Original array: ");
-    for (int i = 0; i < n; i++)
-        printf("%d ", arr[i]);
+    printf("Unsorted array: \n");
+    printArray(arr, n);
 
     heapSort(arr, n);
 
-    printf("\nSorted array: ");
-    for (int i = 0; i < n; i++)
-        printf("%d ", arr[i]);
+    printf("Sorted array: \n");
+    printArray(arr, n);
 
     return 0;
 }
